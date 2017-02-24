6c5e4d284583e31c1705c1dee70c8a435523dc79
pratas <pratas@ua.pt>
2017-02-24 12:23:43 +0000

    cmp

--- run.sh
+++ run.sh
@@ -4,18 +4,23 @@
 # WARNING: YOU NEED APPROX. 100 GB OF FREE DISK
 #
 # RUNNING FLAGS
-GET_GOOSE=1;
-GET_GECO=1;
+GET_GOOSE=0;
+GET_GECO=0;
 #==============================================================================
-GET_HUMAN=1;
-GET_CHIMPANZEE=1;
-GET_GORILLA=1;
-GET_ORANGUTAN=1;
-GET_GIBBON=1;
-GET_ANUBIS=1;
-GET_MARMOSET=1;
+GET_HUMAN=0;
+GET_CHIMPANZEE=0;
+GET_GORILLA=0;
+GET_ORANGUTAN=0;
+GET_GIBBON=0;
+GET_ANUBIS=0;
+GET_MARMOSET=0;
 #==============================================================================
-GET_MTDNA=1;
+GET_MTDNA=0;
+#==============================================================================
+RUN_MITO=1;
+RUN_WGS=0;
+#==============================================================================
+PLOT=1;
 #==============================================================================
 ###############################################################################
 #==============================================================================
@@ -58,7 +63,7 @@ fi
 #==============================================================================
 if [[ "$GET_GORILLA" -eq "1" ]]; then
   . GetGorillaParse.sh
-  rm -f GG24 GG25 
+  rm -f GG24 GG25
   cat GG* > DFGSERGF
   mv DFGSERGF GG
 fi
@@ -72,7 +77,7 @@ fi
 #==============================================================================
 if [[ "$GET_GIBBON" -eq "1" ]]; then
   . GetGibbonParse.sh
-  rm -f GB GB27 GB28 
+  rm -f GB GB27 GB28
   cat GB* > DFGSERGF
   mv DFGSERGF GB
 fi
@@ -96,33 +101,61 @@ if [[ "$GET_MTDNA" -eq "1" ]]; then
   . GetMTDNA.sh
   #
   cat MITO.fna | tr ' ' '_' \
-  | ./goose-extractreadbypattern sapiens_mitoch > MT_HS.fa
+  | ./goose-extractreadbypattern sapiens_mitoch | ./goose-fasta2seq > MT_HS.seq
   cat MITO.fna | tr ' ' '_' \
-  | ./goose-extractreadbypattern troglodytes > MT_PT.fa
+  | ./goose-extractreadbypattern troglodytes | ./goose-fasta2seq > MT_PT.seq
     cat MITO.fna | tr ' ' '_' \
-  | ./goose-extractreadbypattern Gorilla_gorilla_gorilla > MT_GG.fa
+  | ./goose-extractreadbypattern Gorilla_gorilla_gorilla | ./goose-fasta2seq > MT_GG.seq
     cat MITO.fna | tr ' ' '_' \
-  | ./goose-extractreadbypattern abelii > MT_PA.fa
+  | ./goose-extractreadbypattern abelii | ./goose-fasta2seq > MT_PA.seq
   cat MITO.fna | tr ' ' '_' \
-  | ./goose-extractreadbypattern Nomascus_leucogenys > MT_GB.fa
+  | ./goose-extractreadbypattern Nomascus_leucogenys | ./goose-fasta2seq > MT_GB.seq
   cat MITO.fna | tr ' ' '_' \
-  | ./goose-extractreadbypattern Papio_anubis > MT_AN.fa
+  | ./goose-extractreadbypattern Papio_anubis | ./goose-fasta2seq > MT_AN.seq
   cat MITO.fna | tr ' ' '_' \
-  | ./goose-extractreadbypattern Callithrix_jacchus > MT_CJ.fa
+  | ./goose-extractreadbypattern Callithrix_jacchus | ./goose-fasta2seq > MT_CJ.seq
 fi
 #==============================================================================
 ###############################################################################
 #==============================================================================
-
-
-
-
-
-
-
-
-
+if [[ "$RUN_MITO" -eq "1" ]]; then
+  ./GeCo -v -rm 13:200:1:5/10 -rm 10:100:1:0/0 -rm 6:1:0:0/0 -rm 3:1:0:0/0 -c 10 -g 0.9 -r MT_HS.seq MT_HS.seq:MT_PT.seq:MT_GG.seq:MT_PA.seq:MT_GB.seq:MT_AN.seq:MT_CJ.seq &> REPORT_MITO
+fi
 #==============================================================================
 ###############################################################################
 #==============================================================================
-
+if [[ "$RUN_WGS" -eq "1" ]]; then
+  ./GeCo -v -rm 20:200:1:5/10 -rm 14:100:1:0/0 -rm 13:20:0:0/0 -rm 8:1:0:0/0 -c 250 -g 0.9 -r HS HS:PT:GG:PA:GB:AN:CJ &> REPORT_WGS
+fi
+#==============================================================================
+###############################################################################
+#==============================================================================
+if [[ "$PLOT" -eq "1" ]]; then
+  cat REPORT_MITO | grep "compressed bytes" | awk '{ print $12;}' | tail -n 6 | awk '{printf("%d\t%s\n", NR, $0)}' > MT_VALUES
+  cat MT_VALUES
+    cat REPORT_WGS | grep "compressed bytes" | awk '{ print $12;}' | tail -n 6 | awk '{printf("%d\t%s\n", NR, $0)}' > WGS_VALUES;
+  cat WGS_VALUES
+  gnuplot << EOF
+  reset
+  set terminal pdfcairo enhanced color font 'Verdana,12'
+  set output "APE.pdf"
+  set style line 101 lc rgb '#000000' lt 1 lw 4
+  set border 3 front ls 101
+  set tics nomirror out scale 0.75
+  set format '%g'
+  set size ratio 0.8
+  set key outside center top
+  set xtics ("Chimpanzee" 1, "Gorilla" 2, "Orangutan" 3, "Gibbon" 4, "Babbon" 5, "Marmoset" 6)
+  set yrange [0:1] 
+  set xrange [0.5:6.5] 
+  set ytics 0.2
+  set grid 
+  set xtics rotate by -30
+  set ylabel "NRS"
+  set xlabel "Species"
+  set border linewidth 1.5
+  set style line 1 lc rgb '#0060ad' lt 1 lw 4 pt 5 ps 0.4 # --- blue
+  set style line 2 lc rgb '#dd181f' lt 1 lw 4 pt 7 ps 0.5 # --- red
+  plot "MT_VALUES"  with linespoints ls 1 title "mitochondrial", "WGS_VALUES" with linespoints ls 2 title "nuclear"
+EOF
+fi

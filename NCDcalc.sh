9a0459af065f1a2b0b170785bd8f811b632697a4
pratas <pratas@ua.pt>
2018-01-26 15:22:41 +0000

    NCD calc

---
+++ NCDcalc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+NCD=0;
+Cx=`cat  $1 | grep "Total bytes" | awk '{print $3;}'`;
+Cy=`cat  $2 | grep "Total bytes" | awk '{print $3;}'`;
+Cxy=`cat $3 | grep "Total bytes" | awk '{print $3;}'`;
+if (( $Cx < $Cy )); 
+then
+  NCD=`echo "scale=8; ($Cxy - $Cx) / $Cy" | bc -l`;
+else
+  NCD=`echo "scale=8; ($Cyx - $cy) / $Cx" | bc -l`;
+fi
+printf "$ncd\n";

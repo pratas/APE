e724a8d591d6577913d95cd97a3e76d2914269ce
Diogo Pratas <pratas@ua.pt>
2018-05-10 11:23:34 +0100

    data

--- runReferenceFreeComparison.sh
+++ runReferenceFreeComparison.sh
@@ -1,43 +1,19 @@
 #!/bin/bash
 ###############################################################################
-INSTALL_DEPENDENCIES=0;
-#
-INSTALL_DELIMINATE=1;
-INSTALL_MFCOMPRESS=1;
-INSTALL_GECO=1;
+INSTALL_DELIMINATE=0;
+INSTALL_MFCOMPRESS=0;
+INSTALL_GECO=0;
 ###############################################################################
-DOWNLOAD=1;
-PARSE=1;
+DOWNLOAD=0;
+PARSE=0;
 ###############################################################################
-RUN_DELIMINATE=1;
-RUN_MFCOMPRESS=1;
-RUN_GECO=1;
+RUN_DELIMINATE=0;
+RUN_MFCOMPRESS=0;
+RUN_GECO=0;
 ###############################################################################
 RUN_JOIN=1;
 RUN_PLOT=1;
 ###############################################################################
-#
-if [[ "$INSTALL_DEPENDENCIES" -eq "1" ]]; then
-  # GET 7ZIP ==================================================================
-  P7ZIPPATH="projects/p7zip/files/latest";
-  rm -f FILES
-  wget http://sourceforge.net/$P7ZIPPATH/download?source=typ_redirect \
-  -O FILES.tar.bz2
-  tar -xjf FILES.tar.bz2
-  cd p7zip_15.14.1/
-  make all
-  cd ..
-  # GET ZLIB ==================================================================
-  rm -f zlib_1.2.8.dfsg.orig.tar.gz
-  wget \
-  https://launchpad.net/ubuntu/+archive/primary/+files/zlib_1.2.8.dfsg.orig.tar.gz
-  tar -xzf zlib_1.2.8.dfsg.orig.tar.gz
-  cd zlib-1.2.8/
-  ./configure
-  make
-  cd ..
-fi
-#
 ###############################################################################
 #
 function Parse {
@@ -45,20 +21,11 @@ function Parse {
   cat HEADER XTMP > datasets/$1;
   }
 #
-function RunDeliminate {
-  cp ../../datasets/$1 $1.fa
-  rm -f $1.fa.dlim
-  (time ./delim a $1.fa ) &> ../../results/C_DEL_$1
-  ls -la $1.fa.dlim > ../../results/BC_DEL_$1
-  cd ..
-  }
-#
 function RunMFCompress {
   cp ../../datasets/$1 $1.fa
-  rm -f chimpanze.fna.mfc
-  (time ./MFCompressC -v -o chimpanze.fna.mfc chimpanze.fna ) &> ../../results/C_MFC_$1
-  ls -la chimpanze.fna.mfc > ../../results/BC_MFC_$1
-  cd ..
+  rm -f $1.fa.mfc
+  (time ./MFCompressC -v -o $1.fa.mfc $1.fa ) &> ../../results/C_MFC_$1
+  ls -la $1.fa.mfc  | awk '{ print $5;}' > ../../results/BC_MFC_$1
   }
 #
 function RunGeCo {
@@ -68,7 +35,6 @@ function RunGeCo {
   (time ./GeCo $PARAM $1 ) &> ../../results/C_GECO_$1
   ls -la $1.co | awk '{ print $5;}' > ../../results/BC_GECO_$1
   rm -f $1;
-  cd ..
   }
 #
 ###############################################################################
@@ -76,16 +42,6 @@ function RunGeCo {
 mkdir -p datasets
 mkdir -p progs
 cd progs/
-###############################################################################
-# GET DELIMINATE ===============================================================
-if [[ "$INSTALL_GREEN" -eq "1" ]]; then
-  rm -f DELIMINATE_LINUX_64bit.tar.gz;
-  DELSEV="metagenomics.atc.tcs.com/Compression_archive";
-  wget http://$DELSEV/DELIMINATE_LINUX_64bit.tar.gz
-  tar -xzf DELIMINATE_LINUX_64bit.tar.gz
-  mv EXECUTABLES deliminate
-  rm -f DELIMINATE_LINUX_64bit.tar.gz;
-fi
 ###############################################################################
 # GET MFCOMPRESS ==============================================================
 if [[ "$INSTALL_MFCOMPRESS" -eq "1" ]]; then
@@ -159,31 +115,6 @@ if [[ "$PARSE" -eq "1" ]]; then
   fi
 #
 # RUN =========================================================================
-if [[ "$RUN_DELIMINATE" -eq "1" ]]; then
-  echo "Running DELIMINATE ...";
-  mkdir -p results
-  cd progs/deliminate/
-  #
-  RunDeliminate "HS5"
-  RunDeliminate "PT5"
-  RunDeliminate "GG5"
-  #
-  RunDeliminate "HS9"
-  RunDeliminate "PT9"
-  RunDeliminate "GG9"
-  #
-  RunDeliminate "HS13"
-  RunDeliminate "PT13"
-  RunDeliminate "GG13"
-  #
-  RunDeliminate "HS17"
-  RunDeliminate "PT17"
-  RunDeliminate "GG17"
-  # 
-  cd ../../
-  echo "Done!";
-fi
-#==============================================================================
 if [[ "$RUN_MFCOMPRESS" -eq "1" ]]; then
   echo "Running MFCompress ...";
   mkdir -p results
@@ -238,7 +169,6 @@ fi
 # PLOT
 if [[ "$RUN_JOIN" -eq "1" ]]; then
   rm -f DATAPN;
-  printf "Deliminate\t%s\n" `cat results/BC_DEL_HS5 results/BC_DEL_PT5 results/BC_DEL_GG5 results/BC_DEL_HS9 results/BC_DEL_PT9 results/BC_DEL_GG9 results/BC_DEL_HS13 results/BC_DEL_PT13 results/BC_DEL_GG13 results/BC_DEL_HS17 results/BC_DEL_GG17 results/BC_DEL_GG17 | awk '{s+=$1}END{print s}'` >> DATAPN
   printf "MFCompress\t%s\n" `cat results/BC_MFC_HS5 results/BC_MFC_PT5 results/BC_MFC_GG5 results/BC_MFC_HS9 results/BC_MFC_PT9 results/BC_MFC_GG9 results/BC_MFC_HS13 results/BC_MFC_PT13 results/BC_MFC_GG13 results/BC_MFC_HS17 results/BC_MFC_GG17 results/BC_MFC_GG17 | awk '{s+=$1}END{print s}'` >> DATAPN
   printf "GeCo\t%s\n" `cat results/BC_GECO_HS5 results/BC_GECO_PT5 results/BC_GECO_GG5 results/BC_GECO_HS9 results/BC_GECO_PT9 results/BC_GECO_GG9 results/BC_GECO_HS13 results/BC_GECO_PT13 results/BC_GECO_GG13 results/BC_GECO_HS17 results/BC_GECO_GG17 results/BC_GECO_GG17 | awk '{s+=$1}END{print s}'` >> DATAPN
 fi
@@ -247,17 +177,16 @@ fi
 # PLOT
 if [[ "$RUN_PLOT" -eq "1" ]]; then
   echo "set terminal pdfcairo enhanced color
-  set output 'bytes.pdf'
+  set output 'bytes2.pdf'
   set auto
   set boxwidth 0.45
   set xtics nomirror
   set style fill solid 1.00
   set ylabel 'Bytes'
   set xlabel 'Methods'
-  # set yrange[0:180000000]
-  # Lighter grid lines
   set grid ytics lc rgb '#C0C0C0'
   unset key
+  set yrange[0:350000000]
   set grid
   set format y '%.0s %c'
   set style line 2 lc rgb '#406090'
